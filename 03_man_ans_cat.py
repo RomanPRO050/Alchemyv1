4df40a5ad021959b8112f177e208846ee2246977
RomanPRO050 <boev.rs@gmail.com>
2021-09-25 20:40:42 +0300

    Initial commit

---
+++ 03_man_ans_cat.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+
+from random import randint
+from termcolor import cprint
+
+
+# Доработать практическую часть урока lesson_007/python_snippets/08_practice.py
+
+# Необходимо создать класс кота. У кота есть аттрибуты - сытость и дом (в котором он живет).
+# Кот живет с человеком в доме.
+# Для кота дом характеризируется - миской для еды и грязью.
+# Изначально в доме нет еды для кота и нет грязи.
+
+# Доработать класс человека, добавив методы
+#   подобрать кота - у кота появляется дом.
+#   купить коту еды - кошачья еда в доме увеличивается на 50, деньги уменьшаются на 50.
+#   убраться в доме - степень грязи в доме уменьшается на 100, сытость у человека уменьшается на 20.
+# Увеличить кол-во зарабатываемых человеком денег до 150 (он выучил пайтон и устроился на хорошую работу :)
+
+# Кот может есть, спать и драть обои - необходимо реализовать соответствующие методы.
+# Когда кот спит - сытость уменьшается на 10
+# Когда кот ест - сытость увеличивается на 20, кошачья еда в доме уменьшается на 10.
+# Когда кот дерет обои - сытость уменьшается на 10, степень грязи в доме увеличивается на 5
+# Если степень сытости < 0, кот умирает.
+# Так же надо реализовать метод "действуй" для кота, в котором он принимает решение
+# что будет делать сегодня
+
+# Человеку и коту надо вместе прожить 365 дней.
+
+# TODO здесь ваш код
+
+
+class Man:
+    def __init__(self, name):
+        self.name = name
+        self.fullness = 50
+        self.house = None
+
+    def __str__(self):
+        return 'Я - {}, сытость {}'.format(
+            self.name, self.fullness)
+
+    def eat(self):
+        if self.house.food >= 10:
+            print('{} поел'.format(self.name))
+            self.fullness += 10
+            self.house.food -= 10
+        else:
+            print('{} нет еды'.format(self.name), color='red')
+
+    def work(self):
+        cprint('{} сходил на работу'.format(self.name), color='grey')
+        self.house.money += 50
+        self.fullness -= 10
+
+    def play_F1_2021(self):
+        cprint('{} играл в F1 2021 целый день на XBOX'.format(self.name), color='green')
+        self.fullness -= 10
+
+    def shopping(self):
+        if self.house.money >= 50:
+            cprint('{} сходил в магазин за едой'.format(self.name), color='magenta')
+            self.house.money -= 50
+            self.house.food += 50
+        else:
+            print('{} деньги кончились!'.format(self.name), color='red')
+
+    def act(self):
+        if self.fullness <= 0:
+            print('{} умер...'.format(self.name), color='red')
+            return
+        dice = randint(1, 6)
+        if self.fullness < 20:
+            self.eat()
+        elif self.house.food < 10:
+            self.shopping()
+        elif self.house.money < 50:
+            self.work()
+        elif dice == 1:
+            self.work()
+        elif dice == 2:
+            self.eat()
+        else:
+            self.play_F1_2021()
+
+    def move_into_the_house(self, house):
+        self.house = house
+        cprint('{} заехал в дом!!!!'.format(self.name), color='cyan')
+        self.fullness -= 10
+
+
+class House:
+    def __init__(self):
+        self.food = 20
+        self.money = 50
+
+    def __str__(self):
+        return 'В доме еды осталось {}, денег осталось {}'.format(self.food, self.money)
+
+
+vasya = Man(name='Вася')
+my_sweet_home = House()
+vasya.move_into_the_house(house=my_sweet_home)
+
+for day in range(1, 366):
+    cprint('=================== день {} ==================='.format(day), color='yellow')
+    vasya.act()
+    cprint('------------------в конце дня--------------------'.format(day))
+    print(vasya)
+    print(my_sweet_home)
+
+# Усложненное задание (делать по желанию)
+# Создать несколько (2-3) котов и подселить их в дом к человеку.
+# Им всем вместе так же надо прожить 365 дней.
+
+# (Можно определить критическое количество котов, которое может прокормить человек...)
